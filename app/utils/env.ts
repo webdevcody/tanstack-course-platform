5f1ebc5f968af5b60d84dd32f5f56458bb06d8e3
Cody Seibert <webdevcody@gmail.com>
2025-03-30 10:08:33 -0400

    remove unused env

--- app/utils/env.ts
+++ app/utils/env.ts
@@ -3,7 +3,6 @@ export const env = {
   GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID!,
   GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET!,
   HOST_NAME: process.env.HOST_NAME!,
-  STORAGE_BUCKET_NAME: process.env.STORAGE_BUCKET_NAME!,
   NODE_ENV: process.env.NODE_ENV!,
   STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY!,
   STRIPE_PRICE_ID: process.env.STRIPE_PRICE_ID!,
